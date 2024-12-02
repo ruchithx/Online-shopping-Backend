c097304c57db523f0e9b13fcb02a6ee1f4c24a90
MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>
2024-12-02 09:40:11 +0530

    add test cases for product service

--- Product/src/main/java/com/product/product/service/ProductService.java
+++ Product/src/main/java/com/product/product/service/ProductService.java
@@ -37,8 +37,17 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-        return productRepository.findAll();
+        try {
+            List<Product> data = productRepository.findAll();
+            return data;
+        } catch (Exception e) {
+            // Log the exception for debugging
+            System.err.println("Error fetching products: " + e.getMessage());
+            // Optionally, throw a custom exception to inform the client about the error
+            throw new RuntimeException("Failed to fetch products", e);
+        }
     }
+
     public List<Brand> getAllBrands() {
         return brandRepository.findAll();
 //        return modelMapper.map(brandRepository.findAll(), new TypeToken<List<BrandDTO>>(){}.getType());
